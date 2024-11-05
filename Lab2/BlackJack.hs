e0971bbb7bc229b50e5337970e509bb1173f7bb5
luc.samain <luc.samain2@gmail.com>
2024-11-05 17:23:14 +0100

    Lab2 A2 ok

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -24,7 +24,19 @@ displayCard :: Card -> String
 displayCard (Card (Numeric n) s) = show n ++ " of " ++ show s
 displayCard (Card r s)           = show r ++ " of " ++ show s
 
--- Recusively displaying hand by display its cards
+-- Recusively displaying hand by displaying its cards
 display :: Hand -> String
 display Empty       = ""
 display (Add c h) = displayCard c ++ "\n" ++ display h
+
+
+-- A2
+-- Computing the value of a card (10 for higher cards and numeric value for the others)
+valueCard :: Card -> Integer
+valueCard (Card (Numeric n) s) = n
+valueCard (Card r s) = 10
+
+-- Recursively computing the value of a hand by computing the value of each of its cards
+value :: Hand -> Integer
+value Empty = 0
+value (Add c h) = valueCard c + value h
