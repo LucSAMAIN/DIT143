9f444a384af9f0f295ff09665d3d17a9eae00535
luc.samain <luc.samain2@gmail.com>
2024-11-06 13:40:17 +0100

    A2 Lab2 ok mais moche

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -17,6 +17,11 @@ sizeSteps = [ size hand2
             , 1 + 1 + 0
             ,2]
 
+hand3 = Add (Card Ace Spades) (Add (Card Ace Clubs) Empty)
+hand4 = Add (Card Ace Spades) (Add (Card Ace Clubs) (Add (Card Ace Hearts) Empty))
+hand5 = Add(Card Ace Spades) (Add (Card King Clubs) Empty)
+hand6 = Add(Card King Spades) (Add (Card Queen Clubs) (Add (Card Jack Hearts) Empty))
+hand7 = Add(Card King Spades) (Add (Card (Numeric 6) Hearts) Empty)
 
 -- A1
 -- Display a card depending on its rank
@@ -35,7 +40,7 @@ display (Add c h) = displayCard c ++ "\n" ++ display h
 -- numeric value for the others)
 valueCard :: Card -> Integer
 valueCard (Card (Numeric n) s) = n
-valueCard (Card (Ace r) s) = 11
+valueCard (Card Ace s) = 11
 valueCard (Card r s) = 10
 
 -- Recursively computing the value of a hand by computing 
@@ -47,14 +52,18 @@ initialValue (Add c h) = valueCard c + value h
 -- Recursively computing the number of aces in a hand
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
-numberOfAces (Add (Ace c) h) = 1 + numberOfAces h
+numberOfAces (Add (Card Ace _) h) = 1 + numberOfAces h
 numberOfAces (Add c h) = numberOfAces h 
 
 -- Using the last two functions to compute the acutal value of the hand 
 -- (depending of the 21 score)
 value :: Hand -> Integer
-value hand | score < 21 = score 
-           | nbAces > 0 = score -10
+value hand | score < 22 = score 
+            -- the score is too high, can we decrease it using aces?
+           | nbAces > 0 && (score-10) < 21 = score + (-10)
+           | nbAces > 1 && (score-20) < 22 = score + (-20)
+           | nbAces > 2 && (score-30) < 22 = score + (-30)
+           | nbAces > 3 && (score-40) < 22 = score + (-40)
            | otherwise = score
            where score = initialValue hand
                  nbAces = numberOfAces hand
