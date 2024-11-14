87f410862744f98faa68fed7543cf6610e72be5c
Loan Gatimel <loan.gatimel@grenoble-inp.org>
2024-11-14 15:08:03 +0100

    finish

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -4,6 +4,7 @@ module BlackJack where
 import Cards
 import RunGame
 import Test.QuickCheck
+import System.Random
 
 -- A0
 hand2 = Add (Card (Numeric 2) Hearts)
@@ -26,14 +27,20 @@ hand7 = Add(Card King Spades) (Add (Card (Numeric 6) Hearts) Empty)
 -- A1
 -- Display a card depending on its rank
 displayCard :: Card -> String
-displayCard (Card (Numeric n) s) = show n ++ " of " ++ show s
-displayCard (Card r s)           = show r ++ " of " ++ show s
+displayCard (Card (Numeric n) s)| s == Hearts =  show n ++ " \9829"
+                                | s == Spades =  show n ++ " \9824"
+                                | s == Diamonds =  show n ++ " \9830"
+                                | s == Clubs =  show n ++ " \9827"
+
+displayCard (Card r s)  | s == Hearts =  show r ++ " \9829"
+                        | s == Spades =  show r ++ " \9824"
+                        | s == Diamonds =  show r ++ " \9830"
+                        | s == Clubs =  show r ++ " \9827"
 
 -- Recusively displaying hand by displaying its cards
 display :: Hand -> String
 display Empty       = ""
 display (Add c h) = displayCard c ++ "\n" ++ display h
-
 
 -- A2
 -- Computing the value of a card (10 for higher cards, 11 for ace,  
@@ -78,3 +85,93 @@ winner handGuest handBank | gameOver handGuest && gameOver handBank = Bank
                           -- None of the hands have value > 21
                           | value handGuest > value handBank = Guest
                           | otherwise = Bank -- if draw then bank wins anyway haha
+
+-- B1 
+(<+) :: Hand -> Hand -> Hand
+Empty <+ Empty = Empty
+Empty <+ h2 = h2
+h1 <+ Empty = h1
+(Add c h1) <+ h2 = Add c (h1 <+ h2)
+
+
+-- linked properties for Quicheck on B1:
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool 
+prop_onTopOf_assoc p1 p2 p3 = p1<+(p2<+p3) == (p1<+p2)<+p3
+
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf p1 p2 = size p1 + size p2 == size (p1<+p2)
+
+-- B2
+-- Generate all cards of a given suit
+suitHand :: Suit -> Hand
+suitHand s = foldr Add Empty [Card r s | r <- allRanks]
+
+-- List of all ranks
+allRanks :: [Rank]
+allRanks = [Numeric n | n <- [2..10]] ++ [Jack, Queen, King, Ace]
+
+-- Generate a full deck by combining all suit hands
+fullDeck :: Hand
+fullDeck = suitHand Hearts <+ suitHand Spades <+ suitHand Diamonds <+ suitHand Clubs
+
+-- B3
+draw :: Hand -> Hand -> (Hand,Hand)
+draw Empty _ = error "draw: The deck is empty."
+draw (Add c rest) h1 = (rest, (Add c h1))
+
+-- B4
+playBankHelper :: Hand -> Hand -> Hand
+playBankHelper deck bankHand | value bankHand < 16 = playBankHelper smallerDeck biggerHand
+                             | otherwise = bankHand
+    where (smallerDeck, biggerHand) = draw deck bankHand
+
+playBank :: Hand -> Hand
+playBank deck = playBankHelper deck Empty
+
+-- B5
+removeNthCard :: Integer -> Hand -> (Hand, Card) -- takes nth card, deck and returns (new deck, removed card)
+removeNthCard i deck    | i >= size deck = error "removeNthCard: index cannot be superior to the size of the deck."
+                        | i < 0         = error "removeNthCard : index cannot be negative."
+removeNthCard i (Add c deck) | i == 0 = (deck, c)
+                             | otherwise = ((Add c newDeck), removedCard)
+                            where (newDeck, removedCard) = removeNthCard (i-1) deck
+
+shuffleDeck :: StdGen -> Hand -> Hand
+shuffleDeck _ Empty = Empty  
+shuffleDeck g deck = shuffleHelper g deck Empty
+
+shuffleHelper :: StdGen -> Hand -> Hand -> Hand
+shuffleHelper _ Empty shuffled = shuffled 
+shuffleHelper g deck shuffled = 
+    let (index, g1) = randomR (0, (size deck) - 1) g  
+        (newDeck, card) = removeNthCard index deck  
+    in shuffleHelper g1 newDeck (Add card shuffled) 
+
+
+-- properties linked to B5:
+belongsTo :: Card -> Hand -> Bool 
+c `belongsTo` Empty = False 
+c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
+
+
+prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool 
+prop_shuffle_sameCards g c h = c `belongsTo` h == c `belongsTo` shuffleDeck g h
+
+prop_size_shuffle :: StdGen -> Hand -> Bool -- to do
+prop_size_shuffle g deck = (size (shuffleDeck g deck)) == (size deck)
+
+
+-- B6
+implementation = Interface
+  { iFullDeck = fullDeck
+  , iValue    = value
+  , iDisplay  = display
+  , iGameOver = gameOver
+  , iWinner   = winner 
+  , iDraw     = draw
+  , iPlayBank = playBank
+  , iShuffle  = shuffleDeck
+  }
+
+main :: IO () 
+main = runGame implementation
